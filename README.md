a6cf4e90969af4e902b05bba9e97e3924ae41c0f
Jonathan <jon_mcdevitt@hotmail.com>
2017-01-13 10:02:29 -0400

    Testing some git functions with these modifications.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # csci3130-project
+UPDATE: Testing git stuff by adding in a java library for JSON handling.
---
+++ src/Client.java
@@ -0,0 +1,7 @@
+import com.fasterxml.jackson.databind.*;
+
+public class Client {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+    }
+}
